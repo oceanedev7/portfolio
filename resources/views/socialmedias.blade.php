0560032df5eb89134ad9799c8b9ff26eaed3676e
oceanedev7 <oceanepy7@gmail.com>
2025-04-27 19:27:16 -0400

    modification des curseurs des carousels

--- resources/views/socialmedias.blade.php
+++ resources/views/socialmedias.blade.php
@@ -2,7 +2,7 @@
 
 @section('main')
 
-<div class="w-full h-screen flex items-center justify-center space-x-24 overflow-y-hidden">
+<div class="w-full h-screen flex items-center justify-center space-x-48 overflow-y-hidden">
 
     <div class="flex flex-col">
     <div id="default-carousel" class=" relative w-full" data-carousel="slide">
@@ -40,7 +40,7 @@
         </div>
        
         <!-- Slider controls -->
-        <button type="button" class="absolute top-28 start-0 z-30 flex items-center justify-center cursor-pointer group focus:outline-none" data-carousel-prev>
+        <button type="button" class="absolute top-28 right-72 z-30 flex items-center justify-center cursor-pointer group focus:outline-none" data-carousel-prev>
             <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-blue-navy dark:bg-gray-800/30 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                 <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
@@ -48,7 +48,7 @@
                 <span class="sr-only">Previous</span>
             </span>
         </button>
-        <button type="button" class="absolute  top-28 end-0 z-30 flex items-center justify-center cursor-pointer group focus:outline-none" data-carousel-next>
+        <button type="button" class="absolute  top-28 left-72 z-30 flex items-center justify-center cursor-pointer group focus:outline-none" data-carousel-next>
             <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-blue-navy dark:bg-gray-800/30 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                 <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
@@ -109,7 +109,7 @@
         </div>
         
         <!-- Slider controls -->
-        <button type="button" class="absolute top-28  start-0 z-30 flex items-center justify-center cursor-pointer group focus:outline-none" data-carousel-prev>
+        <button type="button" class="absolute top-28 right-[270px] z-30 flex items-center justify-center cursor-pointer group focus:outline-none" data-carousel-prev>
             <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-blue-navy dark:bg-gray-800/30  group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                 <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
@@ -117,14 +117,33 @@
                 <span class="sr-only">Previous</span>
             </span>
         </button>
-        <button type="button" class="absolute top-28 end-0 z-30 flex items-center justify-center cursor-pointer group focus:outline-none" data-carousel-next>
+        <button type="button" class="absolute top-28 left-[270px] z-30 flex items-center justify-center cursor-pointer group focus:outline-none" data-carousel-next>
             <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-blue-navy dark:bg-gray-800/30 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                 <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
                 </svg>
                 <span class="sr-only">Next</span>
             </span>
-        </button>
+        </button> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
     <div class="text-center font-bold text-blue-navy uppercase"> West Indies Barber </div>
 </div>
